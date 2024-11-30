bd622fb64a7ee3c5f3e5ac0b7aeb3b8127c7649b
Milen I. <milenthrowaway381@gmail.com>
2024-11-30 08:44:55 +0200

    Created Pets ORM entity

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -4,11 +4,25 @@ namespace App\Controller;
 
 use App\Controller\Controller as BaseController;
 use Symfony\Component\HttpFoundation\Response as Response;
+use Doctrine\DBAL\DriverManager;
 
+/**
+ * Just one controller for this small list app
+ */
 class Home extends BaseController
 {
     public function index(): Response
     {
+        $connectionParams = [
+            'dbname' => 'tutorial',
+            'user' => 'admin',
+            'password' => 'admin',
+            'host' => 'localhost',
+            'driver' => 'pdo_mysql',
+        ];
+        $conn = DriverManager::getConnection($connectionParams);
+        var_dump($conn);
+
         // phpinfo();exit;
         return $this->render('/pets/add_pet.html.twig');
     }
---
+++ src/Entity/Pets.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PetsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PetsRepository::class)
+ */
+class Pets
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $info;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $modified;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getInfo(): ?string
+    {
+        return $this->info;
+    }
+
+    public function setInfo(?string $info): self
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+
+    public function getCreated(): ?int
+    {
+        return $this->created;
+    }
+
+    public function setCreated(int $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getModified(): ?int
+    {
+        return $this->modified;
+    }
+
+    public function setModified(int $modified): self
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+}
