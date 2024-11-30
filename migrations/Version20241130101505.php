a229bd72ca60718a5f92fdfa9ca94b14177cbc27
Milen I. <milenthrowaway381@gmail.com>
2024-11-30 12:56:50 +0200

    I read migrations should be repod, so here they are

---
+++ migrations/Version20241130101505.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241130101505 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pets CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL, CHANGE created created INT UNSIGNED NOT NULL, CHANGE modified modified INT UNSIGNED NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pets CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE created created INT NOT NULL, CHANGE modified modified INT NOT NULL');
+    }
+}
